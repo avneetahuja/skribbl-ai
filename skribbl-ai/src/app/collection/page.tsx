feb3ed5166f64cc156851c1eb4136c93c77d5c92
avneetahuja <aahuja16@asu.edu>
2024-03-28 22:10:00 -0700

    Working v0

--- skribbl-ai/src/app/collection/page.tsx
+++ skribbl-ai/src/app/collection/page.tsx
@@ -1,19 +1,45 @@
 "use client";
-
+import { api } from "../../../convex/_generated/api";
 import { useQuery } from "convex/react";
-import { Key } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
-  const saveSketchMutation = useQuery("sketches:getSketches");
-  console.log(saveSketchMutation);
+  const sketches = useQuery(api.sketches.getSketches);
+
+  // const sortedSketches = (sketches ?? []).sort((a, b) => {
+  //   return b._creationTime - a._creationTime;
+  // });
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        {saveSketchMutation.map(sketch => (
-          <div key={sketch._id}>{sketch.prompt}</div>
-        ))}
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-purple-300">
+      <h2>Recent Sketches (Only last 1 hour)</h2>
+      <div className="grid grid-cols-4 gap-4">
+        {sketches?.map((sketch) => {
+          // Calculate the time difference in milliseconds
+          const creationTime = new Date(sketch._creationTime);
+          const currentTime = new Date();
+          const timeDifference = currentTime.getTime() - creationTime.getTime();
+
+          // Check if the time difference is less than 24 hours (in milliseconds)
+          const twentyFourHoursInMilliseconds = 60 * 60 * 1000; // 24 hours in milliseconds
+          if (timeDifference < twentyFourHoursInMilliseconds) {
+            return (
+              <div key={sketch._id}>
+                <img
+                width="500"
+                height="500"
+                src={sketch.result}
+              />
+              {/* <Image src={sketch.result} alt="Iamge" width={500} height={500}></Image> */}
+              <p>{sketch.prompt}</p>
+                </div>
+
+            );
+          }
+        })}
       </div>
+      <Link href={"../"}>Back</Link>
     </main>
   );
 }
--- skribbl-ai/src/app/page.tsx
+++ skribbl-ai/src/app/page.tsx
@@ -1,32 +1,103 @@
 "use client";
-import { useMutation } from "convex/react";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { api } from "../../convex/_generated/api";
+import { useMutation, useQuery } from "convex/react";
+import { useForm } from "react-hook-form";
+import { ReactSketchCanvas, ReactSketchCanvasRef } from "react-sketch-canvas";
+import { useRef, useState } from "react";
+import { Input } from "./components/ui/input";
+import { Button } from "./components/ui/button";
+import { Label } from "./components/ui/label";
+import Link from "next/link";
 
 export default function Home() {
-  const saveSketchMutation = useMutation("sketches:saveSketch");
+  const saveSketchMutation = useMutation(api.sketches.saveSketch);
+  const sketchesQuery = useQuery(api.sketches.getSketches);
+
+  const [color, setColor] = useState("black");
 
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm<{
     prompt: string;
   }>();
+
+  const canvasRef = useRef<ReactSketchCanvasRef>(null);
+
+  const sortedSketches = (sketchesQuery ?? []).sort((a, b) => {
+    return b._creationTime - a._creationTime;
+  });
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <form
-        onSubmit={handleSubmit(async (formData) => {
-          console.log(formData);
-          const res = await saveSketchMutation(formData);
-          console.log(res);
-        })}
-      >
-        <input {...register("prompt", { required: true })} />
-        {errors.prompt && <span>This field is required</span>}
-        <br></br>
-        <input type="submit" />
-      </form>
+    <main className="flex bg-purple-300 min-h-screen flex-col items-center pt-8">
+      <div className="text-5xl pb-8">
+        <p>✏️SKRIBBL.AI🤖</p>
+      </div>
+      <div className=" mx-auto flex gap-4 px-8">
+        <form
+          className="flex flex-col gap-2"
+          onSubmit={handleSubmit(async (formData) => {
+            if (!canvasRef.current) return;
+            const image = await canvasRef.current.exportImage("jpeg");
+            await saveSketchMutation({ ...formData, image });
+          })}
+        >
+          <div className="text-4xl">
+            <Label htmlFor="prompt">Prompt</Label>
+            <Input
+              id="prompt"
+              {...register("prompt", { required: true })}
+              style={{ backgroundColor: "white" }}
+            />
+            {errors.prompt && <span className="text-lg">This field is required</span>}
+            <br></br>
+            <Label className="mt-4">Canvas (Draw something below)</Label>
+          </div>
+
+          <ReactSketchCanvas
+            ref={canvasRef}
+            style={{ width: 1200, height: 500 }}
+            strokeWidth={6}
+            strokeColor={color}
+            eraserWidth={26}
+          />
+          <div className="flex flex-row justify-between">
+            <Button
+              type="button"
+              variant={"destructive"}
+              onClick={() => {
+                canvasRef.current?.clearCanvas();
+              }}
+            >
+              ❌
+            </Button>
+            <Button
+              type="button"
+              variant={"destructive"}
+              onClick={() => {
+                canvasRef.current?.eraseMode(true);
+              }}
+            >
+              Eraser
+            </Button>
+            <Button
+              type="button"
+              variant={"destructive"}
+              onClick={() => {
+                canvasRef.current?.eraseMode(false);
+              }}
+            >
+              ✏️
+            </Button>
+            <Button type="button" variant={"ghost"}>
+              <Link href={"/collection"}>Collection</Link>
+            </Button>
+
+            <Button type="submit">Submit</Button>
+          </div>
+        </form>
+      </div>
     </main>
   );
 }
